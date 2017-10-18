256d782a250b63950295d9fe51b6ba0c734ddbaf
laurentiu.marcut <laurent.marcut@mango.com>
2017-10-18 15:39:25 +0200

    Add Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,56 @@
+node {
+
+    String REPOSITORY_URL = "https://github.com/Marcut-Laurentiu/kata-jenkins.git"
+    String SLACK_URL = "https://cop-mango-tech.slack.com/services/hooks/jenkins-ci/"
+    String SLACK_CHANNEL = "#kata_jenkins"
+
+    try {
+        stage('Preparation') {
+            git(url: REPOSITORY_URL, branch: "lau-solution")
+            env.PATH = "${env.PATH}:${tool 'Maven 3.5.0'}/bin"
+        }
+
+        stage('Build') {
+            sh "mvn clean package"
+        }
+
+        stage('Results') {
+            junit '**/target/surefire-reports/TEST-*.xml'
+            archive 'target/*.jar'
+        }
+
+        stage('Upload to S3') {
+            withAWS(region: 'eu-west-1', credentials: 'AWS_CREDENTIALS') {
+                s3Upload(bucket: "kata-jenkins",
+                        path: "my-name/kata-jenkins.jar",
+                        includePathPattern: 'target/*.jar'
+                )
+            }
+        }
+
+        stage('Notification') {
+            withCredentials([string(credentialsId: 'SLACK_TOKEN', variable: 'SLACK_TOKEN')]) {
+                slackSend baseUrl: SLACK_URL,
+                        token: SLACK_TOKEN,
+                        channel: SLACK_CHANNEL,
+                        color: '#00FF00',
+                        message: """
+                        SUCCESS: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'
+                    """
+            }
+        }
+    } catch (e) {
+        withCredentials([string(credentialsId: 'SLACK_TOKEN', variable: 'SLACK_TOKEN')]) {
+            slackSend baseUrl: SLACK_URL,
+                    token: "f00Ytla44JHVGcxoHuEgvEF8",
+                    channel: SLACK_CHANNEL,
+                    color: '#FF0000',
+                    message: """
+                    FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'\n
+                    Error: $e
+                """
+        }
+        currentBuild.result = "FAILED"
+        throw e
+    }
+}
